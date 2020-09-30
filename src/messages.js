66fdaf485dd574d73ca097124853d9a2a9a2a1a7
Kyle Leong <kyle.leong@thecarousell.com>
2020-09-30 13:04:44 +0800

    Update messages.js

--- src/messages.js
+++ src/messages.js
@@ -2,10 +2,10 @@ export const messages = [
   // Add your message in the following format
   // Remember to add your picture files in the /public folder
   // {
-  //   title: "Hello Yao Hui",
-  //   name: "Ah bob",
-  //   msg: "See u in Seattle",
-  //   coverImg: "./ahbobcover.jpg",
-  //   profileImg: "./ahbobprofile.jpg",
+  //   title: "Hello Bui",
+  //   name: "John",
+  //   msg: "See you on steam",
+  //   coverImg: "./johncover.jpg",
+  //   profileImg: "./johnprofile.jpg",
   // },
 ];
